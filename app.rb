b3dacd08087b086dcc6bbdbc19e58ab7aacdb2ca
Michael Arledge <arledgemike@gmail.com>
2013-12-27 17:19:38 -0600

    rick james

--- app.rb
+++ app.rb
@@ -4,7 +4,26 @@ require './enviroments'
 
 
 class Post < ActiveRecord::Base 
+end  
+  get "/" do 
+    @posts= Post.order("created_at DESC")
+    @title = "Tell someone to eat a bag of dicks"
+    erb :"post/index"
+  end
+
+  get "/post/eabod/:from/:to" do
+    @from = params[:from]
+    @to = params[:to]
+    erb :"post/eabod"
+  end
 
 
-
-end
+  helpers do 
+    def title
+      if @title
+        "#{@title}"
+      else
+        "Welcome"
+      end
+    end
+  end
