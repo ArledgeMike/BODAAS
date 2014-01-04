dd121d93868eb990411c933126629a4a90197e62
ArledgeMike <arledgemike@gmail.com>
2014-01-04 12:55:05 -0600

    functionally working needs styles

--- Gemfile
+++ Gemfile
@@ -2,17 +2,14 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra'
-gem 'activerecord'
-gem 'sinatra-activerecord'
-gem 'sinatra-flash'
-gem 'sinatra-redirect-with-flash'
+
 
 group :development do 
-  gem 'sqlite3'
-  gem 'tux'
+  gem 'pg'
 end
 
 group :production do
   gem 'pg'
+  gem 'rails_12factor'
 end
 
--- app.rb
+++ app.rb
@@ -1,29 +1,46 @@
 require 'sinatra'
-require 'sinatra/activerecord'
-require './enviroments'
 
-
-class Post < ActiveRecord::Base 
-end  
   get "/" do 
-    @posts= Post.order("created_at DESC")
     @title = "Tell someone to eat a bag of dicks"
-    erb :"post/index"
+    erb :"/index"
   end
 
-  get "/post/eabod/:from/:to" do
+  get "/apiconsole" do
+    @title = "Use the console"   
+    erb :"apiconsole"
+  end
+
+  get "/apipost" do
+    @to = params[:to]
+    @curse = params[:verb]
+    @from = params[:from]
+    @url = "localhost:4567/hey/#{@to}/#{@curse}/#{@from}/"
+    @url = @url.gsub(" ", "%20")
+    erb :"/hey"
+  end  
+  
+
+  get "/hey/:to/:verb/:from" do
     @from = params[:from]
     @to = params[:to]
-    erb :"post/eabod"
+    @curse = params[:verb] 
+    @url = "localhost:4567/hey/#{@to}/#{@curse}/#{@from}/"
+    @url = @url.gsub(" ", "%20")
+    erb :"/hey"
   end
+
 
 
   helpers do 
     def title
       if @title
-        "#{@title}"
+        @title
       else
-        "Welcome"
+       @title = "EABOD API"
       end
     end
+    
+    include Rack::Utils
+    alias_method :h, :escape_html
+    
   end
